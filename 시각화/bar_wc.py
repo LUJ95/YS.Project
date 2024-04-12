4247a6b4f2254256f970facb0f948be71252ef6c
Juchan Park <pjc6269@gmail.com>
2024-04-12 11:17:59 +0900

    발표스크립트 추가, 시각화 모듈 수정

--- 시각화/bar_wc.py
+++ 시각화/bar_wc.py
@@ -6,55 +6,77 @@ Created on Tue Apr  9 10:14:33 2024
 """
 
 # 막대그래프, 가로 막대그래프 함수 모듈 :
-    # bar_wordcount(x_col, y_col, filename)
-    # barh_wordcount(x축 컬럼, y축 컬럼, 저장할 파일 이름)
+    # bar_wordcount(df, 저장할 파일 이름)
+    # barh_wordcount(df, 저장할 파일 이름)
 
 # %%
-def bar_wordcount(x_col, y_col, filename):
-    
-    import matplotlib.pyplot as plt
-    plt.rc('font', family='NaNumBarunGothic')
-    plt.rcParams['figure.dpi'] = 140
-    
-    bar = plt.bar(x_col, y_col, width=0.6)
-    plt.title('단어별 빈도수')
-    plt.xticks(rotation=55)
-    # 막대 위 값 표시
-    for rect in bar:
-        height = rect.get_height()
-        plt.text(rect.get_x() + rect.get_width()/2.0, height, '%.1d' % height, ha='center', va='bottom', size = 8, color='r')
-    plt.savefig(filename + '_bar')
-    plt.show()
-# %%
-def barh_wordcount(x_col, y_col, filename):
-    
-    import matplotlib.pyplot as plt
-    plt.rc('font', family='NanumBarunGothic')
-    plt.rcParams['figure.dpi'] = 140
-    
-    barh = plt.barh(x_col, y_col, height=0.6)
-    plt.title('단어별 빈도수')
-    plt.yticks(fontsize=8)
-    for rect in barh:
-        width = rect.get_width()
-        plt.text(width + 1.5, rect.get_y() - 0.15, '%.1d' % width, ha='center', va='bottom', size = 7, color='g')
-    plt.savefig(filename + '_barh')
-    plt.show()
-    
+def bar_wordcount(df, filename):
+    try:
+        import matplotlib.pyplot as plt
+        make_bar(df['word'], df['count'])
+        plt.title('단어별 빈도수')
+        plt.savefig(filename + '_bar')
+        plt.show()
+    except Exception as e:
+        print(e)
 
+# %%
+def make_bar(x_col, y_col):
+    try:
+        import matplotlib.pyplot as plt
+        plt.rc('font', family='NaNumBarunGothic')
+        plt.rcParams['figure.dpi'] = 140
+        
+        bar = plt.bar(x_col, y_col, width=0.6)
+        
+        plt.xticks(rotation=55)
+        # 막대 위 값 표시
+        for rect in bar:
+            height = rect.get_height()
+            plt.text(rect.get_x() + rect.get_width()/2.0, height, '%.1d' % height, ha='center', va='bottom', size = 8, color='r')        
+    except Exception as e:
+        print(e)
+# %%
+def barh_wordcount(df, filename):
+    try:
+        import matplotlib.pyplot as plt
+        make_barh(df['word'], df['count'])
+        plt.title('단어별 빈도수')
+        plt.savefig(filename + '_barh')
+        plt.show()
+    except Exception as e:
+        print(e)
+
+# %%
+def make_barh(x_col, y_col):
+    try:
+        import matplotlib.pyplot as plt
+        plt.rc('font', family='NanumBarunGothic')
+        plt.rcParams['figure.dpi'] = 140
+        
+        barh = plt.barh(x_col, y_col, height=0.6)
+        
+        plt.yticks(fontsize=8)
+        for rect in barh:
+            width = rect.get_width()
+            plt.text(width + 1.5, rect.get_y() - 0.15, '%.1d' % width, ha='center', va='bottom', size = 7, color='g')
+    except Exception as e:
+        print(e)
 # %%
 if __name__ == "__main__":
     
     import pandas as pd # 데이터 프레임 라이브러리
     import numpy as np # 행렬 라이브러리
     
-    # import matplotlib.font_manager as fm
+    import matplotlib.font_manager as fm
 
     # font_files = fm.findSystemFonts(fontpaths=['C:/Windows/Fonts'])
-    # font_files = fm.findSystemFonts(fontpaths=['C:/Users/jcp/AppData/Local/Microsoft/Windows/Fonts'])
+    # # font_files = fm.findSystemFonts(fontpaths=['C:/Users/jcp/AppData/Local/Microsoft/Windows/Fonts'])
     # for fpath in font_files:
         
     #     fm.fontManager.addfont(fpath)
+        
+    fm.fontManager.addfont('./NanumBarunGothic.ttf')
 
     df = pd.read_csv('한산_댓글모음.csv')
     df
@@ -89,5 +111,5 @@ if __name__ == "__main__":
 
     top20 = df_word.head(20)
     
-    bar_wordcount(top20['word'], top20['count'])
-    barh_wordcount(top20['word'], top20['count'])
+    bar_wordcount(top20,'한산_댓글모음')
+    barh_wordcount(top20,'한산_댓글모음')
