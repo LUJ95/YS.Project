937059ddcb72a8684c2cbfe65884942e82472c6a
chance6269 <117437659+chance6269@users.noreply.github.com>
2024-04-13 12:29:45 +0900

    전처리 함수 예외처리 수정

--- 시각화/pp.py
+++ 시각화/pp.py
@@ -44,24 +44,30 @@ def preprocess(df):
         # 영문자, 숫자, 한글, 공백을 제외한 모든 문자를 찾아 제거
         pattern = r'[^a-zA-Z가-힣\s]'
         text = re.sub(pattern, '', text)  
-        
+        print('패턴 제거 완료')
         # 불용어 제거
         delete_patterns = ['니다', '국민', '여러분', '청년', '감사', '생각', '노력', '존경']
         for pattern in delete_patterns:
             text = re.sub(pattern, '', text)
-     
+        print('불용어 제거 완료')
         # 명사만 추출하기
         komoran = Komoran()
         nouns = komoran.nouns(text)
-    
+        print('명사추출 완료')
         # 단어 빈도 개수 세기
         df_words_counts = Counter(nouns)
-        
+        print('빈도 개수 세기 완료')
         # 1개 미만 단어 제외 및 내림차순 정렬
         df = df_words_counts.most_common()
         df = pd.DataFrame(df, columns=["word", "count"])
         selected = df['word'].str.len() > 1
         return df[selected]
-
     except Exception as e:
         print(e)
+        empty = []
+        return pd.DataFrame({'연설문':empty})
+    
+        
+    
+        
+        
